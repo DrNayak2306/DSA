963a392f8d5a504240f9f1405d4faf4dffccba29
DrNayak2306 <dhruvgirishnayak@gmail.com>
2025-05-14 15:39:55 +0530

    Second largest of an array

---
+++ second_largest.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    int arr[n];
+    for (int i=0; i<n; i++){
+        cin >> arr[i];
+    }
+    int first, second;
+
+    // // better solution
+    // first = second = -1;
+    // for (int i=0; i<n; i++){
+    //     if (arr[i] > second){
+    //         second = arr[i];
+    //     }
+    //     if (second > first){
+    //         int temp = first;
+    //         first = second;
+    //         second = temp;
+    //     }
+    // }
+    
+    // optimal solution
+    first = second = -1;
+    for (int i=0; i<n; i++){
+        if (arr[i] > first){
+            second = first;
+            first = arr[i];
+        }
+        else if (arr[i] > second){
+            second = arr[i];
+        }
+    }
+
+    cout << second << endl;
+    return 0;
+}
