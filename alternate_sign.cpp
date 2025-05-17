c6d4e6eda77d1771aca0eed93726d62cc66f85d6
DrNayak2306 <dhruvgirishnayak@gmail.com>
2025-05-17 11:35:24 +0530

    One pass, two pointers

--- alternate_sign.cpp
+++ alternate_sign.cpp
@@ -9,18 +9,41 @@ int main(){
     for (int i=0; i<n; i++){
         cin >> arr[i];
     }
-    vector<int> res(n);
-    int pos(0), neg(1);
+    vector<int> res;
+
+    // brute-force
+    list<int> posarr, negarr;
     for (int i=0; i<n; i++){
-        if (arr[i] < 0){
-            res[neg] = arr[i];
-            neg+=2;
-        }
-        else{
-            res[pos] = arr[i];
-            pos+=2;
-        }
+        if (arr[i] > 0) posarr.push_back(arr[i]);
+        else negarr.push_back(arr[i]);
     }
+    while (not posarr.empty() && not negarr.empty()){
+        res.push_back(posarr.front());
+        posarr.pop_front();
+        res.push_back(negarr.front());
+        negarr.pop_front();
+    }
+    while (posarr.size() > 0){
+        res.push_back(posarr.front());
+        posarr.pop_front();
+    }
+    while (negarr.size() > 0){
+        res.push_back(negarr.front());
+        negarr.pop_front();
+    }
+
+    // optimal
+    // int pos(0), neg(1);
+    // for (int i=0; i<n; i++){
+    //     if (arr[i] < 0){
+    //         res[neg] = arr[i];
+    //         neg+=2;
+    //     }
+    //     else{
+    //         res[pos] = arr[i];
+    //         pos+=2;
+    //     }
+    // }
     for (int i=0; i<n; i++){
         cout << res[i] << " ";
     }
--- main.cpp
+++ main.cpp
@@ -5,10 +5,25 @@ using namespace std;
 int main(){
     int n;
     cin >> n;
-    int x;
+    vector<int> arr(n);
     for (int i=0; i<n; i++){
-        cin >> x;
-        cout << x*x << endl;
+        cin >> arr[i];
     }
+    vector<int> res(n);
+    int pos(0), neg(1);
+    for (int i=0; i<n; i++){
+        if (arr[i] < 0){
+            res[neg] = arr[i];
+            neg+=2;
+        }
+        else{
+            res[pos] = arr[i];
+            pos+=2;
+        }
+    }
+    for (int i=0; i<n; i++){
+        cout << res[i] << " ";
+    }
+    cout << endl;
     return 0;
 }
