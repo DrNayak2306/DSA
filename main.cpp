d60c973a8866d5f1e1319da573a6d3339100789e
DrNayak2306 <dhruvgirishnayak@gmail.com>
2025-05-17 10:34:50 +0530

    build task for main.cpp added

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    int x;
+    for (int i=0; i<n; i++){
+        cin >> x;
+        cout << x*x << endl;
+    }
+    return 0;
+}
